27cfe9e5a59235d2f3fc4824f4c398077db02151
GregT <taylor.gregory2@gmail.com>
2022-06-08 20:25:42 -0600

    commit tree 355044ddfbdcbfd783aa1ef6f0d3b0f72c4fce9b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map {|a| "Hello, my name is #{a}."}
+    end
+
+def assign_rooms arr
+    t = arr.length - 1
+    while t > 0 do
+    newarr = []
+    arr.each_with_index do | n, i |
+        i = i + 1
+       newarr += "Hello, #{n}! You'll be assigned to room #{i}!"
+       t += 1
+        end
+        end
+        newarr
+    end
